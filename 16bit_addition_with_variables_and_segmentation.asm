e5e033e382cf38b8a4db396451adff72cc81053d
Subhransu S Mohanty <68157462+er-ssmohanty@users.noreply.github.com>
2021-11-30 20:21:11 +0530

    Add files via upload

---
+++ 16bit_addition_with_variables_and_segmentation.asm
@@ -0,0 +1,17 @@
+forest segment
+    var1 dw 1234H
+    var2 dw 2315H
+    var3 dw ?
+    var4 dw ?
+forest ENDS
+moor segment
+    assume DS:forest
+start:  mov AX,forest
+    mov DS,AX 
+    mov AX,0000H
+    add Ax,var1
+    add AX,var2
+    mov var3, AX
+    hlt               
+moor ENDS                         
+end start
---
+++ README.md
@@ -0,0 +1,6 @@
+# 8085assembly
+Assembly programs for intel 8085 microprocessor
+
+All these programs are tested and verified in GNUsim8085(debian).
+You are most welcome to test these programs on different emulators, training boards and platforms and send pull requests to modify codes 
+and add comments. 
